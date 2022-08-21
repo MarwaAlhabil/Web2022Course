88be238b17bc91f3458a101f1481830a9817ad49
MarwaAlhabil <109893929+MarwaAlhabil@users.noreply.github.com>
2022-08-21 12:31:11 +0300

    commit

---
+++ form.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+
+<form action="" method="get">
+Name: <input type="text" name="name"><br>
+Email: <input type="text" name="email"><br>
+passw: <input type="text" name="password"><br>
+
+Gender:<br>
+<input type="radio" name="gender" value="female">Female<br>
+<input type="radio" name="gender" value="male">Male<br>
+<input type="submit"><br>
+
+
+</form>
+
+</body>
+</html>
