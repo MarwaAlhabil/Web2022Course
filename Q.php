4966b5a169d97fba43e1f1009cd0504616cffeac
MarwaAlhabil <109893929+MarwaAlhabil@users.noreply.github.com>
2022-08-23 10:16:32 +0300

    commit

---
+++ Q.php
@@ -0,0 +1,62 @@
+<?php
+
+// for($i=1;$i<=10;$i++){
+//     if($i<10 ){
+//     echo " $i - ";
+    
+//     }
+//     else{
+//         echo " $i"."\n";
+//     }
+// }
+
+// for($i=1;$i<=5;$i++){
+//     for($x=1;$x<=$i;$x++){
+//         echo"* \t ";
+//     }
+//     echo"<br>";
+// }
+
+// for($i=1;$i<=5;$i++){
+//     for($x=6-$i;$x>=1;$x--){
+//         echo"* \t";
+//     }
+//     echo"<br>";
+// }
+
+// for( $i =0; $i <5; $i++ ) {
+//     for($j=0; $j < 4; $j++) {
+//       if($i === 2 ) {
+//         echo('*');
+//       }
+//        else {
+//         if($j === 1 || $j === 2) {
+//           echo('&nbsp;&nbsp;');
+//         } else {
+//           echo('*');
+//         }
+//       }
+      
+//     }
+//     echo('<br />');
+//   }
+
+
+
+
+    // $num=3;
+    // $f=1;
+    // for($i=$num;$i>=1;$i--){
+    //     $f=$f*$i;
+    // }
+    // echo" the factoril of $num = $f ";
+
+    $c=5;
+    while($c<=15){
+        echo"the number is : $c <br>";
+        $c++;
+    }
+
+
+?>
+
